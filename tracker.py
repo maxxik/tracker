1288bb54c5d7e01dfc340ae688513cf3f15f767d
Marton Gyulai <gyulai.marton@outlook.com>
2018-02-01 01:19:06 +0100

    #3 Color, difference detection

--- tracker.py
+++ tracker.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture('DSC_0352.MOV')
+cap = cv2.VideoCapture('DSC_0353.MOV')
 
 # Define the codec and create VideoWriter object
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
@@ -16,23 +16,57 @@ if(cap.isOpened()):
 
 while(cap.isOpened()):
 
+    # 1) DIFFERENCE DETECTION
     ret, frame2 = cap.read()
 
     gray2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
 
     diff = cv2.absdiff(gray1, gray2)
 
-    thresh = cv2.threshold(diff, 50, 255, cv2.THRESH_BINARY)[1]
-    
-    edges = cv2.Canny(frame, 100, 200)
+    thresh = cv2.threshold(diff, 60, 255, cv2.THRESH_BINARY)[1]
 
-    final = cv2.addWeighted()
+    _, contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    print(len(contours))
+    contourimg = cv2.drawContours(thresh, contours, -1, (127,186,50), 1)
+
+    for contour in contours:
+        x,y,w,h = cv2.boundingRect(contour)
+        cv2.rectangle(frame2,(x,y),(x+w,y+h),(0,255,0),2)
+
+    # 2) COLOR DETECTION
+    hsv = cv2.cvtColor(frame2, cv2.COLOR_BGR2HSV)
+
+    # define range of blue color in HSV
+    trshld_low = np.array([-33,100,100])
+    trshld_high = np.array([33,255,255])
+
+    # Threshold the HSV image to get only blue colors
+    mask = cv2.inRange(hsv, trshld_low, trshld_high)
+
+    thresh_color = cv2.threshold(mask, 100, 255, cv2.THRESH_BINARY)[1]
+
+    kernel = np.ones((20,20),np.uint8)
+    dilated = cv2.dilate(thresh_color,kernel,iterations = 1)
+    cv2.imshow('dilated', dilated)
+
+
+    # Bitwise-AND mask and original image
+    masked = cv2.bitwise_and(frame2, frame2, mask = thresh_color)
+
+
+    
+    #edges = cv2.Canny(frame2, 150, 200)
+
+    #final = cv2.addWeighted(thresh, 1, edges, 1, 0)
 
     cv2.imshow('Frame1', frame1)
     cv2.imshow('Frame2', frame2)
-    cv2.imshow('Difference', thresh)
+    cv2.imshow('Coloured', masked)
+    cv2.imshow('Mask', mask)
+    cv2.imshow('thresh_color', thresh_color)
+    #cv2.imshow('Final', final)
 
-    out.write(diff)
+    out.write(thresh)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
