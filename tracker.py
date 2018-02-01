1362736db431b5828ce02652b38e6a217294169a
Marton Gyulai <gyulai.marton@outlook.com>
2018-02-01 22:58:48 +0100

    #4 Color detection improved, tracking started

--- tracker.py
+++ tracker.py
@@ -1,13 +1,17 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture('DSC_0353.MOV')
+cap = cv2.VideoCapture('GOPR1152.MP4')
 
 # Define the codec and create VideoWriter object
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output.avi', fourcc, 29.97, (640,240))
+#out = cv2.VideoWriter('output.avi', fourcc, 29.97, (640,240))
 frame = []
 gray1 = []
+
+green_track = []
+red_track = []
+plot = np.zeros((720, 1280, 3), np.uint8)
 
 if(cap.isOpened()):
     ret, frame1 = cap.read()
@@ -16,8 +20,10 @@ if(cap.isOpened()):
 
 while(cap.isOpened()):
 
-    # 1) DIFFERENCE DETECTION
     ret, frame2 = cap.read()
+
+    '''
+    # 1) DIFFERENCE DETECTION
 
     gray2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
 
@@ -32,44 +38,80 @@ while(cap.isOpened()):
     for contour in contours:
         x,y,w,h = cv2.boundingRect(contour)
         cv2.rectangle(frame2,(x,y),(x+w,y+h),(0,255,0),2)
+    '''
 
     # 2) COLOR DETECTION
-    hsv = cv2.cvtColor(frame2, cv2.COLOR_BGR2HSV)
-
-    # define range of blue color in HSV
-    trshld_low = np.array([-33,100,100])
-    trshld_high = np.array([33,255,255])
-
-    # Threshold the HSV image to get only blue colors
-    mask = cv2.inRange(hsv, trshld_low, trshld_high)
-
-    thresh_color = cv2.threshold(mask, 100, 255, cv2.THRESH_BINARY)[1]
-
-    kernel = np.ones((20,20),np.uint8)
-    dilated = cv2.dilate(thresh_color,kernel,iterations = 1)
-    cv2.imshow('dilated', dilated)
 
 
-    # Bitwise-AND mask and original image
-    masked = cv2.bitwise_and(frame2, frame2, mask = thresh_color)
+    # a) GREEN CUP
+    blurred = cv2.GaussianBlur(frame2, (15, 15),0)
+    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
 
+    # Define range of green color in HSV
+    green_low = np.array([20,80,80])
+    green_high = np.array([45,130,130])
 
+    # Threshold the HSV image to get only green colors
+    green_mask = cv2.inRange(hsv, green_low, green_high)
+
+    # Dilate the points
+    kernel = np.ones((5,5),np.uint8)
+    green_dilated = cv2.dilate(green_mask, kernel, iterations = 1)
     
-    #edges = cv2.Canny(frame2, 150, 200)
+    # Find the contours
+    _, green_contours, _ = cv2.findContours(green_dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    #print("Green contours: " + str(len(green_contours)))
+    #contourimg = cv2.drawContours(dilated, contours, -1, (127,186,50), 1)
 
-    #final = cv2.addWeighted(thresh, 1, edges, 1, 0)
+    #Draw bounding rectangles around the curvevs
+    for contour in green_contours:
+        x,y,w,h = cv2.boundingRect(contour)
+        cv2.rectangle(frame2,(x,y),(x+w,y+h),(0,255,0),2)
+        green_track.append((x + w/2, y + h/2))
+        plot = cv2.circle(plot, ((int) (x + w/2), (int) (y + h/2)), 3, (0,255,0), 2 )
+        
+
+
+
+
+    # b) RED RIBBON
+    blurred = cv2.GaussianBlur(blurred, (25, 25),0)
+    # Define range of red color in HSV
+    red_low = np.array([160,40,40])
+    red_high = np.array([180,140,140])
+
+    # Threshold the HSV image to get only red colors
+    red_mask = cv2.inRange(hsv, red_low, red_high)
+
+    # Dilate the points
+    kernel = np.ones((15,15),np.uint8)
+    red_dilated = cv2.dilate(red_mask, kernel, iterations = 1)
+    
+    # Find the contours
+    _, red_contours, _ = cv2.findContours(red_dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    #print("Red contours: " + str(len(red_contours)))
+    #contourimg = cv2.drawContours(dilated, contours, -1, (127,186,50), 1)
+
+    #Draw bounding rectangles around the curvevs
+    for contour in red_contours:
+        x,y,w,h = cv2.boundingRect(contour)
+        cv2.rectangle(frame2,(x,y),(x+w,y+h),(0,0,255),2)
+        red_track.append((x + w/2, y + h/2))
+        plot = cv2.circle(plot, ((int) (x + w/2), (int) (y + h/2)), 3, (0,0,255), 2 )
+
+
 
     cv2.imshow('Frame1', frame1)
     cv2.imshow('Frame2', frame2)
-    cv2.imshow('Coloured', masked)
-    cv2.imshow('Mask', mask)
-    cv2.imshow('thresh_color', thresh_color)
-    #cv2.imshow('Final', final)
+    cv2.imshow('Path', plot)
 
-    out.write(thresh)
+    #out.write(thresh)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+print(red_track)
+print(green_track)
 
 # Release everything if job is finished
 cap.release()
